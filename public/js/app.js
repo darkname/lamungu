1252246ec9414b965d099fd0e9a267897ce08408
Benjamin Lofo Follo <benjamin.lofofollo@hotmail.com>
2016-05-11 17:19:41 -0400

    Change Background and Side Nav

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,12 @@
-var app = angular.module("lamungu", ['angularSmoothscroll']);
+var app = angular.module("lamungu", ['angularSmoothscroll','ui.bootstrap']);
+app.controller('HomeController', ['$scope','$window',function($scope, $window) {
+    $scope.isCollapsed = true;
 
-//app.controller('HomeController', ['smoothScroll', function(smoothScroll){}]);
+    $scope.$watch(function () {
+        return $window.scrollY;
+    }, function (scrollY) {
+        /* logic */
+        console.log('working');
+        $scope.showTop = (scrollY > 100);
+    });
+}]);
